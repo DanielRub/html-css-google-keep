58381b98c363ec4c04dbf3ff99e400ff42ecb234
daniel rubambura <danielrubambura@gmail.com>
2021-10-04 09:30:56 +0100

    add achive feature

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ class Note {
 class App {
   constructor() {
     this.notes = [];
-
+    this.currentNote;
     this.$inactiveForm = document.querySelector(".inactive-form");
     this.$activeForm = document.querySelector(".active-form");
     this.$activeForm.style.display = "none";
@@ -17,6 +17,8 @@ class App {
     this.$noteText = document.querySelector("#note-text");
     this.$notes = document.querySelector(".notes");
     this.$modal = document.querySelector(".modal");
+    this.$modalNoteTitle = document.querySelector("#modal-note-title");
+    this.$modalNoteText = document.querySelector("#modal-note-text");
 
     this.addEventListeners();
   }
@@ -25,34 +27,68 @@ class App {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
       this.openModal(event);
+      this.archive(event);
     });
 
     this.$activeForm.addEventListener("submit", (event) => {
       event.preventDefault();
       const title = this.$noteTitle.value;
       const text = this.$noteText.value;
-      this.addNote(cuid(), title, text);
+      if (title != "") {
+        this.addNote(cuid(), title, text);
+      }
       this.closeForm();
     });
   }
 
   openModal(event) {
+    event.preventDefault();
     const $selectedNote = event.target.closest(".note");
-    if($selectedNote) 
-        this.$modal.classList.add("open-modal");
+    const $actionBarActivated = event.target.closest(".note-footer");
+    const $modalCloseSelect = event.target.closest(".modal-close");
+
+    if ($selectedNote && !$actionBarActivated) {
+      this.$modal.classList.add("open-modal");
+      const note = this.notes.find((note) => note.id === $selectedNote.id);
+      this.$modalNoteText.value = note.text;
+      this.$modalNoteTitle.value = note.title;
+      this.currentNote = $selectedNote;
+    } else if ($modalCloseSelect && this.currentNote) {
+      this.$modal.classList.remove("open-modal");
+      this.editNote(this.currentNote.id,this.$modalNoteTitle.value,this.$modalNoteText.value);
+      this.currentNote = null;
+      this.renderNotes();
+    }
+  }
+  archive(event) {
+    const $closestNote = event.target.closest(".note");
+    const $archiveAction = event.target.closest(".action-archive");
+    if ($closestNote && $archiveAction) {
+      const id = $closestNote.id;
+      this.deleteNote(id);
+      this.renderNotes();
+    }
   }
 
   handleFormClick(event) {
     const isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
     const activeFormClickedOn = this.$activeForm.contains(event.target);
+    const $actionBarActivated = event.target.closest(".note-footer");
     const title = this.$noteTitle.value;
     const text = this.$noteText.value;
-
+    const compId = event.target.id;
+    console.log(event.target);
     if (isInactiveFormClickedOn) {
       this.openForm();
-    } else if (!activeFormClickedOn && !isInactiveFormClickedOn) {
-      this.addNote(cuid(), title, text);
-      this.closeForm();
+    } else if ((
+      !activeFormClickedOn &&
+      !isInactiveFormClickedOn &&
+      !$actionBarActivated )|| compId=="form-close"
+    ) {
+      if (title != "") {
+        this.addNote(cuid(), title, text);
+        this.closeForm();
+      }
     }
   }
 
@@ -75,7 +111,7 @@ class App {
   }
 
   editNote(id, title, text) {
-    this.notes.map((note) => {
+    this.notes=this.notes.map((note) => {
       if (note.id === id) {
         note.title = title;
         note.text = text;
@@ -91,7 +127,7 @@ class App {
   renderNotes() {
     this.$notes.innerHTML = this.notes
       .map((note) => {
-        return `<div class="note">
+        return `<div class="note" id="${note.id}">
       <span class="material-icons check-circle">check_circle</span>
       <div class="title">${note.title}</div>
       <div class="text">${note.text}</div>
@@ -121,7 +157,7 @@ class App {
           <span class="tooltip-text">Add Image</span>
         </div>
         <div class="tooltip">
-          <span class="material-icons-outlined hover small-icon"
+          <span class="material-icons-outlined hover small-icon action-archive"
             >archive</span
           >
           <span class="tooltip-text">Archive</span>
